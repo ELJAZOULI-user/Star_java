2096d2786844089b620706d6997bab1d91b6eb9f
ELJAZOULI-user <85343839+ELJAZOULI-user@users.noreply.github.com>
2021-06-04 17:14:14 +0200

    adding star sides

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ public class Main {
         int size=5;
         int width=(size*6-1);
         int height=(size*4)+2;
-
-        //for(int i=0;i<(size*4)+2;i++){
-            //top pic
+        //top pic
         for(int l=0;l<size;l++){
             if(l==0){
                 System.out.println(" ".repeat(width/2)+"*");
@@ -13,7 +11,22 @@ public class Main {
                 System.out.println(" ".repeat((size*3)-l-1)+"*"+" ".repeat(l+(l-1))+"*");
             }
         }
+        //line
         System.out.println("*".repeat((size*2)+1)+" ".repeat((size*2)-3)+"*".repeat((size*2)+1));
+        
+        //sides
+        for(int k=1;k<(size*2);k++){
+            if(k<size){
+                System.out.println(" ".repeat(k) + "*" + " ".repeat(width - (k * 2) - 2) + "*");
+            }else{
+                System.out.println(" ".repeat(size*2-k) + "*" + " ".repeat(width - ((size*2-k) * 2) - 2) + "*");
+            }
+        }
+        
+        //line
+        System.out.println("*".repeat((size*2)+1)+" ".repeat((size*2)-3)+"*".repeat((size*2)+1));
+        
+        
     }
 
 }
